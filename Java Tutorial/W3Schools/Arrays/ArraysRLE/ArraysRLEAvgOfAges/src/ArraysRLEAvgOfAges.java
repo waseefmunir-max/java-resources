4b9e69d7ce9d5c7bfc260e336b652e585c15c8a9
waseefmunir-max <waseef05@gmail.com>
2026-05-12 23:53:38 +0600

    Another commit

--- Java Tutorial/W3Schools/Arrays/ArraysRLE/ArraysRLEAvgOfAges/src/ArraysRLEAvgOfAges.java
+++ Java Tutorial/W3Schools/Arrays/ArraysRLE/ArraysRLEAvgOfAges/src/ArraysRLEAvgOfAges.java
@@ -3,9 +3,13 @@ public class ArraysRLEAvgOfAges {
 //        int ages[] = {20,34,56,23,21,60,55,78,31,35};
         int[] ages = {20,34,56,23,21,60,55,78,31,35};
         int sum = 0;
-        double avg = 0;
         for (int agesFor : ages) {
             sum += agesFor;
         }
+
+        float avg = (float) sum / ages.length;
+
+        System.out.println("The average age is: " + avg);
+//        System.out.printf("The average age is: %.2f", avg);
     }
 }
