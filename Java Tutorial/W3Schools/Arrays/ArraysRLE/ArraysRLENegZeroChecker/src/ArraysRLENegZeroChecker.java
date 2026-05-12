80581ec870ea1b69dac16982f79652283630d4c9
waseefmunir-max <waseef05@gmail.com>
2026-05-13 00:01:37 +0600

    Another commit

--- Java Tutorial/W3Schools/Arrays/ArraysRLE/ArraysRLENegZeroChecker/src/ArraysRLENegZeroChecker.java
+++ Java Tutorial/W3Schools/Arrays/ArraysRLE/ArraysRLENegZeroChecker/src/ArraysRLENegZeroChecker.java
@@ -1,5 +1,9 @@
 public class ArraysRLENegZeroChecker {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 5, -7, 9, 0, 11, 13};
+
+        for (int i = 0; i < numbers.length; i++) {
+
+        }
     }
 }
