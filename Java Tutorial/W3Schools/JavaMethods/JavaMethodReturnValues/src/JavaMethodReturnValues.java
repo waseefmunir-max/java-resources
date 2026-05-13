3af6654bb3770bd1e408e67f87759ed2142298bf
waseefmunir-max <waseef05@gmail.com>
2026-05-13 10:05:51 +0600

    Added age checker method

--- Java Tutorial/W3Schools/JavaMethods/JavaMethodReturnValues/src/JavaMethodReturnValues.java
+++ Java Tutorial/W3Schools/JavaMethods/JavaMethodReturnValues/src/JavaMethodReturnValues.java
@@ -4,12 +4,18 @@ public class JavaMethodReturnValues {
     }
 
     static int doubler(int x) {
-        return x *= x;
+        return x *= 2;
     }
 
     public static void main(String[] args) {
         System.out.println(sum2Numbers(2, 3));
         System.out.println(sum2Numbers(2, 4));
         System.out.println(sum2Numbers(3, 5));
+
+        System.out.println();
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("DOuble of " + i + " is " + doubler(i));
+        }
     }
 }
