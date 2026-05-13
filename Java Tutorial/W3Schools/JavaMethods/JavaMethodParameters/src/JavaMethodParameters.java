af7114e606b355b46129ce50b636d7eddc9a41cf
waseefmunir-max <waseef05@gmail.com>
2026-05-13 09:07:50 +0600

    Added age checker method

--- Java Tutorial/W3Schools/JavaMethods/JavaMethodParameters/src/JavaMethodParameters.java
+++ Java Tutorial/W3Schools/JavaMethods/JavaMethodParameters/src/JavaMethodParameters.java
@@ -7,6 +7,16 @@ public class JavaMethodParameters {
         System.out.println(firstName + " Walker," + " age: " + age);
     }
 
+    static void checkAge (int age) {
+        if (age >= 18 && age <= 60) {
+            System.out.println("You are eligible to vote!");
+        } else if (age > 60) {
+            System.out.println("You are a senior citizen");
+        } else {
+            System.out.println("You are not old enough to vote");
+        }
+    }
+
     public static void main(String[] args) {
         firstName("Alan");
         firstName("Paul");
@@ -17,5 +27,11 @@ public class JavaMethodParameters {
         info("Alan", 21);
         info("Paul", 23);
         info("John", 24);
+
+        System.out.println();
+
+        checkAge(20);
+        checkAge(70);
+        checkAge(10);
     }
 }
