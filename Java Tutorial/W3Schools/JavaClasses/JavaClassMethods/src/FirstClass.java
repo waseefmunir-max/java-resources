08f180213790b98d69615c09542d66184b49fff6
waseefmunir-max <waseef05@gmail.com>
2026-05-13 13:44:46 +0600

    Another commit

--- Java Tutorial/W3Schools/JavaClasses/JavaClassMethods/src/FirstClass.java
+++ Java Tutorial/W3Schools/JavaClasses/JavaClassMethods/src/FirstClass.java
@@ -1,2 +1,18 @@
 public class FirstClass {
+//    public static void fullThrottle() {
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
+    }
+
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
+    }
+
+    public static void main(String[] args) {
+        FirstClass myObject = new FirstClass();
+
+        myObject.fullThrottle(); // calling methods just like  variables
+        myObject.speed(180);
+
+    }
 }
