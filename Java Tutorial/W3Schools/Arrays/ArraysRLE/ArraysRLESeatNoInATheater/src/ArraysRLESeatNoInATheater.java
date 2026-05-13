91b8cf86b58be5087a5c05e5afed5fbfe8d65f0b
waseefmunir-max <waseef05@gmail.com>
2026-05-13 08:39:01 +0600

    Another commit

--- Java Tutorial/W3Schools/Arrays/ArraysRLE/ArraysRLESeatNoInATheater/src/ArraysRLESeatNoInATheater.java
+++ Java Tutorial/W3Schools/Arrays/ArraysRLE/ArraysRLESeatNoInATheater/src/ArraysRLESeatNoInATheater.java
@@ -1,6 +1,8 @@
 public class ArraysRLESeatNoInATheater {
     public static void main(String[] args) {
         String[] name = new String[] {"Jenny", "Liam", "Angie", "Bo"};
-
+        for (int i = 0; i < name.length; i++) {
+            System.out.println("Seat number " + i + " is taken by " + name[i]);
+        }
     }
 }
