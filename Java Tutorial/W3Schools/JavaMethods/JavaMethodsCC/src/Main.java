361f220c280cf7feb512d4ea7dc819f23b60dac4
waseefmunir-max <waseef05@gmail.com>
2026-05-13 08:54:16 +0600

    Another commit

--- Java Tutorial/W3Schools/JavaMethods/JavaMethodsCC/src/Main.java
+++ Java Tutorial/W3Schools/JavaMethods/JavaMethodsCC/src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    static void welcomeMessage() {
+        System.out.println("Welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+        welcomeMessage();
+    }
 }
