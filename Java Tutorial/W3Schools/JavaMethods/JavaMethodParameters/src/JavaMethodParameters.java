c46c49aece18e2bddbd15d9ab9fb76680e647b36
waseefmunir-max <waseef05@gmail.com>
2026-05-13 09:02:43 +0600

    Another commit

--- Java Tutorial/W3Schools/JavaMethods/JavaMethodParameters/src/JavaMethodParameters.java
+++ Java Tutorial/W3Schools/JavaMethods/JavaMethodParameters/src/JavaMethodParameters.java
@@ -3,9 +3,19 @@ public class JavaMethodParameters {
         System.out.println(fName + " Walker");
     }
 
+    static void info(String firstName, int age) {
+        System.out.println(firstName + " Walker," + " age: " + age);
+    }
+
     public static void main(String[] args) {
         firstName("Alan");
         firstName("Paul");
         firstName("John");
+
+        System.out.println();
+
+        info("Alan", 21);
+        info("Paul", 23);
+        info("John", 24);
     }
 }
