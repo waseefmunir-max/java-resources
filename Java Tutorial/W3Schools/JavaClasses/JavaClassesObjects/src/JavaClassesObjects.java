36ab06ac80d2c73e06f2721e26dc370ab9e72de0
waseefmunir-max <waseef05@gmail.com>
2026-05-13 13:18:48 +0600

    Another commit

---
+++ Java Tutorial/W3Schools/JavaClasses/JavaClassesObjects/src/JavaClassesObjects.java
@@ -0,0 +1,11 @@
+public class JavaClassesObjects {
+    int x = 5;
+
+    public static void main(String[] args) {
+        JavaClassesObjects myObject1 = new JavaClassesObjects(); // creating an object
+        JavaClassesObjects myObject2 = new JavaClassesObjects(); // creating another object
+
+        System.out.println(myObject1.x);
+        System.out.println(myObject2.x);
+    }
+}
