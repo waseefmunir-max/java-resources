127a2a19b5b3ed6d40114c7fe9fcb7e6567754c0
waseefmunir-max <waseef05@gmail.com>
2026-05-13 13:21:01 +0600

    Another commit

---
+++ Java Tutorial/W3Schools/JavaClasses/JavaClassesObjects/src/First.java
@@ -0,0 +1,11 @@
+public class First {
+    int x = 5;
+
+    public static void main(String[] args) {
+        First myObject1 = new First(); // creating an object
+        First myObject2 = new First(); // creating another object
+
+        System.out.println(myObject1.x);
+        System.out.println(myObject2.x);
+    }
+}
