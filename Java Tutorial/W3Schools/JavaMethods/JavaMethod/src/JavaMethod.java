75a316d1b5d76e1367b65bcd49610828e617315f
waseefmunir-max <waseef05@gmail.com>
2026-05-13 08:44:31 +0600

    Another commit

--- Java Tutorial/W3Schools/JavaMethods/JavaMethod/src/JavaMethod.java
+++ Java Tutorial/W3Schools/JavaMethods/JavaMethod/src/JavaMethod.java
@@ -2,4 +2,8 @@ public class JavaMethod {
     static void aMethod() {
         System.out.println("Hello World!");
     }
+
+    public static void main(String[] args) {
+        aMethod();
+    }
 }
