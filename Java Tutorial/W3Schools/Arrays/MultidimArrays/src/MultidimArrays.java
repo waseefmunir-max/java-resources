47eaa8e37bca8733143bafd4e08b35b3423e580b
waseefmunir-max <waseef05@gmail.com>
2026-05-14 08:26:14 +0600

    Another commit

--- Java Tutorial/W3Schools/Arrays/MultidimArrays/src/MultidimArrays.java
+++ Java Tutorial/W3Schools/Arrays/MultidimArrays/src/MultidimArrays.java
@@ -1,2 +1,11 @@
 public class MultidimArrays {
+    public static void main(String[] args) {
+        int[][] number = {{2, 4, 6, 8}, {1, 3, 5, 7}};
+        for (int numberFor1[] : number) {
+            for (int numberFor2 : numberFor1) {
+                System.out.print(numberFor2 + " ");
+            }
+            System.out.println();
+        }
+    }
 }
