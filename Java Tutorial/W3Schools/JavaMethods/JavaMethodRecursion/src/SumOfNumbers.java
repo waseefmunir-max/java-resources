9a6274dc38989aa22858ffa6b62fd47e9e69bdc8
waseefmunir-max <waseef05@gmail.com>
2026-05-14 20:44:10 +0600

    Another commit

--- Java Tutorial/W3Schools/JavaMethods/JavaMethodRecursion/src/SumOfNumbers.java
+++ Java Tutorial/W3Schools/JavaMethods/JavaMethodRecursion/src/SumOfNumbers.java
@@ -1,2 +1,14 @@
 public class SumOfNumbers {
+    public static int sumOfNumbers(int x) {
+        if (x > 0) {
+            return x + sumOfNumbers(x - 1);
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println("Sum: " + sumOfNumbers(x));
+    }
 }
