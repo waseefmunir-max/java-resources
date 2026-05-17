94cf822312009e61f2dac78a0308fd6ee689dbae
waseefmunir-max <waseef05@gmail.com>
2026-05-17 22:54:02 +0600

    Another commit

--- Java Tutorial/W3Schools/JavaClasses/JavaModifiers/AccessModifiers/src/AMClass1.java
+++ Java Tutorial/W3Schools/JavaClasses/JavaModifiers/AccessModifiers/src/AMClass1.java
@@ -1,15 +1,13 @@
-public class AMClass1 {
+class AMClass {
     public String name = "John Doe"; // Accessible everywhere
-    private int age = 25; // Accessible only in class AMClass1
-
-    public static void main(String[] args) {
-        AMClass1 obj1 = new AMClass1();
-
-        System.out.println("Name: " + obj1.name);
-        System.out.println("Age: " + obj1.age);
-    }
+    private int age = 25; // Accessible only in class AMClass
 }
 
-class AMClass11 {
+public class AMClass1 {
+    public static void main(String[] args) {
+        AMClass obj1 = new AMClass();
 
+        System.out.println("Name: " + obj1.name);
+//        System.out.println("Age: " + obj1.age); // Throws out an error
+    }
 }
