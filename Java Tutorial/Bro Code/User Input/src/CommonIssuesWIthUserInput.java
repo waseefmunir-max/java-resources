703480e18393fa8263ce1de68b1ca69a1bc7230f
waseefmunir-max <waseef05@gmail.com>
2026-05-27 22:56:30 +0600

    Another Commit

---
+++ Java Tutorial/Bro Code/User Input/src/CommonIssuesWIthUserInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class CommonIssuesWIthUserInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+}
