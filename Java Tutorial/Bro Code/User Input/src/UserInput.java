4429546a14d771c6444a558a8eae78ee20ea1dd6
waseefmunir-max <waseef05@gmail.com>
2026-05-27 22:37:24 +0600

    Another Commit

--- Java Tutorial/Bro Code/User Input/src/UserInput.java
+++ Java Tutorial/Bro Code/User Input/src/UserInput.java
@@ -6,6 +6,11 @@ public class UserInput {
         // Here, 'Scanner' is a class that allows us to read user input from the console. We create an object of the 'Scanner' class called 'scanner' and pass 'System.in' as an argument to its constructor. This tells the 'Scanner' to read input from the standard input stream (the console).
         Scanner scanner = new Scanner(System.in);
 
-        scanner.close(); // Good practice to close
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name);
+
+        scanner.close(); // Good practice to close the object
     }
 }
