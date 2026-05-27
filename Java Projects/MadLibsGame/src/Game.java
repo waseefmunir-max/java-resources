dab65e75fd593e569d49dc31987cfb36c5625ea8
waseefmunir-max <waseef05@gmail.com>
2026-05-27 23:23:50 +0600

    Another Commit

--- Java Projects/MadLibsGame/src/Game.java
+++ Java Projects/MadLibsGame/src/Game.java
@@ -7,6 +7,22 @@ public class Game {
         String adjective1, noun1, adjective2, verb1, adjective3;
 
 
+        System.out.print("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
+
+        System.out.print("Enter an noun (animal or person): ");
+        noun1 = scanner.nextLine();
+
+        System.out.print("Enter an adjective (description): ");
+        adjective2 = scanner.nextLine();
+
+        System.out.print("Enter a verb ending with -ing (action): ");
+        verb1 = scanner.nextLine();
+
+        System.out.print("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine();
+
+
 
         scanner.close();
     }
