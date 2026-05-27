be7623cd461b4119c3635fc7fa452c5cd8032a65
waseefmunir-max <waseef05@gmail.com>
2026-05-27 23:19:58 +0600

    Another Commit

--- Java Projects/MadLibsGame/src/Game.java
+++ Java Projects/MadLibsGame/src/Game.java
@@ -4,6 +4,8 @@ public class Game {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String adjective1, noun1, adjective2, verb1, adjective3;
+
 
 
         scanner.close();
