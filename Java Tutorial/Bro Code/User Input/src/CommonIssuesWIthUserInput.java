1bd44588728f89d1bfd2d9739a6a12376ff19314
waseefmunir-max <waseef05@gmail.com>
2026-05-27 22:57:36 +0600

    Another Commit

--- Java Tutorial/Bro Code/User Input/src/CommonIssuesWIthUserInput.java
+++ Java Tutorial/Bro Code/User Input/src/CommonIssuesWIthUserInput.java
@@ -4,6 +4,12 @@ public class CommonIssuesWIthUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter your favorite color: ");
+        String color = scanner.nextLine();
+
         scanner.close();
     }
 }
