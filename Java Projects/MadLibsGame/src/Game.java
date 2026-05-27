168c6dc75c03eba863c2a1231215668d17d15908
waseefmunir-max <waseef05@gmail.com>
2026-05-27 23:28:06 +0600

    Another Commit

--- Java Projects/MadLibsGame/src/Game.java
+++ Java Projects/MadLibsGame/src/Game.java
@@ -4,7 +4,11 @@ public class Game {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String adjective1, noun1, adjective2, verb1, adjective3;
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
 
 
         System.out.print("Enter an adjective (description): ");
@@ -22,7 +26,10 @@ public class Game {
         System.out.print("Enter an adjective (description): ");
         adjective3 = scanner.nextLine();
 
-
+        System.out.println("\nToday I went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was smelly and screaming!");
+        System.out.println("I was " + adjective3 + "!");
 
         scanner.close();
     }
