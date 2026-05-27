6f37d6c3a28c25ebf6cb8c04b19c9062890a4bbd
waseefmunir-max <waseef05@gmail.com>
2026-05-27 23:01:19 +0600

    Another Commit

--- Java Tutorial/Bro Code/User Input/src/CommonIssuesWIthUserInput.java
+++ Java Tutorial/Bro Code/User Input/src/CommonIssuesWIthUserInput.java
@@ -6,9 +6,13 @@ public class CommonIssuesWIthUserInput {
 
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
+        scanner.nextLine(); // Removes the newline character left in the buffer after reading an integer
 
         System.out.print("Enter your favorite color: ");
         String color = scanner.nextLine();
+
+        System.out.println("You're " + age + " old");
+        System.out.println("You like the color " + color);
 
         scanner.close();
     }
