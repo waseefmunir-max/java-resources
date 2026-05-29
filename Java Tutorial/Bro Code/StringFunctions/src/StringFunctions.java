1f98c6c86bebef4c12e7228cbdaeaaf36116b4a2
waseefmunir-max <waseef05@gmail.com>
2026-05-29 11:02:17 +0600

    Another Commit

--- Java Tutorial/Bro Code/StringFunctions/src/StringFunctions.java
+++ Java Tutorial/Bro Code/StringFunctions/src/StringFunctions.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 
 public class StringFunctions {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String text;
+
+        System.out.print("Enter some text: ");
+        text = scanner.nextLine();
+
+        if (text.isEmpty()) {
+            System.out.println("You didn't enter anything!");
+        } else if (text.isBlank()) {
+            System.out.println("You entered whitespaces, but not any texts!");
+        } else {
+            System.out.println("Your entered texts: " + text);
+        }
+
+        // .isEmpty checks if the string is empty (length is 0), and it counts whitespaces as characters, so if the string contains only whitespaces, .isEmpty will return false.
+        // .isBlank checks if the string is empty, but unlike .isEmpty, it ignores whitespaces. So if the string contains only whitespaces, .isBlank will return true.
+
+        scanner.close();
     }
 }
