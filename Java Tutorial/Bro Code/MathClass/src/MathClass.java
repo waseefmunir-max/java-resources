7bff9db74095cc57b1e8c3093b793ecb1632a81c
waseefmunir-max <waseef05@gmail.com>
2026-05-31 20:24:05 +0600

    Another Commit

--- Java Tutorial/Bro Code/MathClass/src/MathClass.java
+++ Java Tutorial/Bro Code/MathClass/src/MathClass.java
@@ -1,2 +1,9 @@
 public class MathClass {
+    public static void main(String[] args) {
+        // Java has built-in Math class that has many useful methods for performing mathematical operations. We can use these methods by calling them with the class name 'Math' followed by a dot '.' and then the method name.
+
+        // For example, let's print the value of PI:
+        System.out.println("Value of PI: " + Math.PI);
+        System.out.println("Value of exponential: " + Math.E);
+    }
 }
