8a29e114ec3331587d64aac011d738d8cfaab61d
waseefmunir-max <waseef05@gmail.com>
2026-05-31 20:31:35 +0600

    Another Commit

---
+++ Java Tutorial/Bro Code/MathClass/src/HypotenuseOfRightTriangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class HypotenuseOfRightTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+
+
+        scanner.close();
+    }
+}
