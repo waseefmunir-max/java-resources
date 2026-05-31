24afe9691f8389de5dd875144ac0bda1b22b7ec6
waseefmunir-max <waseef05@gmail.com>
2026-05-31 20:15:48 +0600

    Another Commit

--- Java Tutorial/Bro Code/RandomNumbers/src/RandomNumbers.java
+++ Java Tutorial/Bro Code/RandomNumbers/src/RandomNumbers.java
@@ -8,8 +8,12 @@ public class RandomNumbers {
         int randomNumber2 = random.nextInt(6); // generates a random number of type int, in the range of 0 to 5 (6 is exclusive, so it will not be included in the range)
         int randomNumber3 = random.nextInt(1, 7); // generates a random number of type int, in the range of 1 to 6 (7 is exclusive, so it will not be included in the range, where 1 is inclusive, so it will be included in the range)
 
+        double randomDouble = random.nextDouble(); // We can generate random numbers for doubles also
+
         System.out.println(randomNumber1);
         System.out.println(randomNumber2);
         System.out.println(randomNumber3);
+
+        System.out.println(randomDouble);
     }
 }
