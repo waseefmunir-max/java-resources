106b27905b83fbc0ddc6f3a3e486f31528b2518e
waseefmunir-max <waseef05@gmail.com>
2026-06-06 15:02:50 +0600

    commit

--- Java Tutorial/Bro Code/StringMethod/src/StringMethod.java
+++ Java Tutorial/Bro Code/StringMethod/src/StringMethod.java
@@ -9,12 +9,14 @@ public class StringMethod {
 
         int index1 = name.indexOf("e");
         int index2 = name.indexOf('s');
-        int index3 = name.indexOf("e", 1); //
+        int index3 = name.indexOf("e", 1); // start searching for "e" from index 1
         int index4 = name.indexOf('s', 2); // start searching for 's' from index 2
+        int index5 = name.indexOf("Munir"); // returns the index of the first character of "Munir"
 
         System.out.println(index1);
         System.out.println(index2);
         System.out.println(index3);
         System.out.println(index4);
+        System.out.println(index5);
     }
 }
