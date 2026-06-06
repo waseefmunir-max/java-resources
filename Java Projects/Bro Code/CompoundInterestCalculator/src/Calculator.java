5a7a52b333e43df559d0eaee5cf4c481c9265a7b
waseefmunir-max <waseef05@gmail.com>
2026-06-06 14:22:38 +0600

    Commit

--- Java Projects/Bro Code/CompoundInterestCalculator/src/Calculator.java
+++ Java Projects/Bro Code/CompoundInterestCalculator/src/Calculator.java
@@ -4,7 +4,8 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double principleAmount, interestRate, numOfCompoundedTimes, numOfYears, amount, compoundInterest;
+        int numOfYears;
+        double principleAmount, interestRate, numOfCompoundedTimes, amount, compoundInterest;
 
         System.out.print("Enter the principle amount: ");
         principleAmount = scanner.nextDouble();
@@ -17,13 +18,13 @@ public class Calculator {
         numOfCompoundedTimes = scanner.nextDouble();
 
         System.out.print("Enter the # of years: ");
-        numOfYears = scanner.nextDouble();
+        numOfYears = scanner.nextInt();
 
         amount = principleAmount * Math.pow((1 + interestRate / numOfCompoundedTimes), (numOfCompoundedTimes * numOfYears));
         compoundInterest = amount - principleAmount;
 
-        System.out.printf("The amount after 1 year(s) is: %.2f", amount);
-        System.out.printf("Compound interest: %.2f\n", compoundInterest);
+        System.out.printf("The amount after %d year(s) is: $%.2f\n", numOfYears, amount);
+        System.out.printf("Compound interest: $%.2f\n", compoundInterest);
 
         scanner.close(); // closing the scanner object to prevent memory leaks
     }
