8f01af4a6f1fb5f196a15bb3ed1d6e6fa494c732
waseefmunir-max <waseef05@gmail.com>
2026-06-09 10:08:46 +0600

    Commit

---
+++ Java Projects/From OOC Lab/Relay/src/main/java/PushMessage.java
@@ -0,0 +1,32 @@
+public class PushMessage extends Message {
+    private static final int PREVIEW_SIZE = 40;
+    private static final double FLAT_COST = 0.01;
+
+    public PushMessage(Recipient recipient, String text) {
+        super(recipient, text);
+    }
+
+    private String preview() {
+        if (getText().length() <= PREVIEW_SIZE) {
+            return getText();
+        }
+
+        return getText().substring(0, PREVIEW_SIZE);
+    }
+
+    @Override
+    public String deliver() {
+        return "PUSH to " + getRecipient().getName()
+                + ": " + preview();
+    }
+
+    @Override
+    public double cost() {
+        return FLAT_COST;
+    }
+
+    @Override
+    public String describe() {
+        return "Push message for " + getRecipient().getName();
+    }
+}
