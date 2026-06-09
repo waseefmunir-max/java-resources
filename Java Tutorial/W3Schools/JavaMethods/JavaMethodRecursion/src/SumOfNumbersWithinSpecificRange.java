2f4678d494caa61912f0b058d857eb352996b8c1
waseefmunir-max <waseef05@gmail.com>
2026-06-09 12:12:52 +0600

    Commit

--- Java Tutorial/W3Schools/JavaMethods/JavaMethodRecursion/src/SumOfNumbersWithinSpecificRange.java
+++ Java Tutorial/W3Schools/JavaMethods/JavaMethodRecursion/src/SumOfNumbersWithinSpecificRange.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SumOfNumbersWithinSpecificRange {
     public static int sumOfNumbers(int start, int end) {
         if (end > start) {
@@ -8,7 +10,19 @@ public class SumOfNumbersWithinSpecificRange {
     }
 
     public static void main(String[] args) {
-        int start = 5, end = 10;
-        System.out.println("Sum of numbers from 5 to 10 is: " + sumOfNumbers(start, end));
+        Scanner scanner = new Scanner(System.in);
+        int start, end;
+
+        System.out.print("Enter the first number: ");
+        start = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter the second number: ");
+        end = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.printf("Sum of numbers from %d to %d is: %d\n", start, end, sumOfNumbers(start, end));
+
+        scanner.close();
     }
 }
