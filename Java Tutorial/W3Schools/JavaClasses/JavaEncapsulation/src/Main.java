b42e64dc0da5226154cf50b198bdaa4d30555b07
waseefmunir-max <waseef05@gmail.com>
2026-06-10 12:01:00 +0600

    Commit

--- Java Tutorial/W3Schools/JavaClasses/JavaEncapsulation/src/Main.java
+++ Java Tutorial/W3Schools/JavaClasses/JavaEncapsulation/src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        Person p = new Person();
+        // This following will cause error as the fields are declared as private
+//        p.name = "John";
+//        p.age = 21;
+//
+//        System.out.println(p.name);
+//        System.out.println(p.age);
+
+        // Instead, we can use getters and setters
+
+    }
 }
--- Java Tutorial/W3Schools/JavaClasses/JavaEncapsulation/src/Person.java
+++ Java Tutorial/W3Schools/JavaClasses/JavaEncapsulation/src/Person.java
@@ -1,4 +1,22 @@
 public class Person {
     private String name;
     private int age;
+
+    // Getters:
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Setters:
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
 }
