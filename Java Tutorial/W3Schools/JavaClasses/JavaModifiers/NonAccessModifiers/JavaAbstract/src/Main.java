7dffeac88651859abb2620eb187f30389546cfde
waseefmunir-max <waseef05@gmail.com>
2026-06-10 11:18:20 +0600

    Commit

--- Java Tutorial/W3Schools/JavaClasses/JavaModifiers/NonAccessModifiers/JavaAbstract/src/Main.java
+++ Java Tutorial/W3Schools/JavaClasses/JavaModifiers/NonAccessModifiers/JavaAbstract/src/Main.java
@@ -1,2 +1,12 @@
-public class Main {
+abstract public class Main {
+    public String name = "John";
+    public int age = 24;
+
+    public abstract void study();
+}
+
+class Student extends Main {
+    public void study() {
+        System.out.println("Studying all day long....");
+    }
 }
---
+++ Java Tutorial/W3Schools/JavaClasses/JavaModifiers/NonAccessModifiers/JavaAbstract/src/Output.java
@@ -0,0 +1,10 @@
+public class Output {
+    public static void main(String[] args) {
+        Student s = new Student(); // creating student object
+
+        System.out.println("Name: " + s.name);
+        System.out.println("Age: " + s.age);
+        System.out.print("Status: ");
+        s.study();
+    }
+}
