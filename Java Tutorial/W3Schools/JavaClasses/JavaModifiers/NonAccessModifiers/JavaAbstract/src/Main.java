7b93631c0ede968c249bd867eb7f079918967080
waseefmunir-max <waseef05@gmail.com>
2026-06-10 11:28:53 +0600

    Commit

--- Java Tutorial/W3Schools/JavaClasses/JavaModifiers/NonAccessModifiers/JavaAbstract/src/Main.java
+++ Java Tutorial/W3Schools/JavaClasses/JavaModifiers/NonAccessModifiers/JavaAbstract/src/Main.java
@@ -3,10 +3,20 @@ abstract public class Main {
     public int age = 24;
 
     public abstract void study();
+
+    public void study2() { // methods can be declared (with their body) without abstract
+
+    }
+
+//    public abstract void animalSound(); // Will cause an error if BOTH study() and animalSound() are not implemented in the child class
 }
 
 class Student extends Main {
     public void study() {
         System.out.println("Studying all day long....");
     }
+}
+
+abstract class Animal {
+
 }
