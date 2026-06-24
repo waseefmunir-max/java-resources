0e0ecad8038214476a7f57a9192fff60af1fa227
waseefmunir-max <waseef05@gmail.com>
2026-06-24 16:01:39 +0600

    CallAConstrFromAnothConstructor.java: Add this keyword to call the 2-parameterized constructor

--- Java Tutorial/W3Schools/JavaClasses/JavaThisKeyword/src/CallAConstrFromAnothConstructor.java
+++ Java Tutorial/W3Schools/JavaClasses/JavaThisKeyword/src/CallAConstrFromAnothConstructor.java
@@ -5,8 +5,13 @@ public class CallAConstrFromAnothConstructor {
 
     public CallAConstrFromAnothConstructor(String brandName, String modelName) {
 //        brandName = "Hello"; // This line will cause an error
-        this(2000, brandName, modelName); // Calling the constructor with 3 parameters from the constructor with 2 parameters. Here, we are passing the value of releaseYear as 2000, and the values of brandName and modelName are being passed from the parameters of this constructor.
+        // Calling the 3-parameterized constructor:
+//        this(2000, brandName, modelName); // Calling the constructor with 3 parameters from the constructor with 2 parameters. Here, we are passing the value of releaseYear as 2000, and the values of brandName and modelName are being passed from the parameters of this constructor.
         // Note that while calling a constructor from another constructor of same class, the calling line has to be the first line, otherwise it will throw an error
+        // Or declaring values here, like this:
+        this.releaseYear = 2000;
+        this.brandName = brandName;
+        this.modelName = modelName;
     }
 
     public CallAConstrFromAnothConstructor(int releaseYear, String brandName, String modelName) {
@@ -21,9 +26,9 @@ public class CallAConstrFromAnothConstructor {
     }
 
     public static void main(String[] args) {
-        CallAConstrFromAnothConstructor carObj1 = new CallAConstrFromAnothConstructor("BMW", "GTR"); // It will call the 1st constructor (at line 5)
+        CallAConstrFromAnothConstructor carObj1 = new CallAConstrFromAnothConstructor("BMW", "GTR"); // It will call the 1st constructor (at line 6)
 
-        CallAConstrFromAnothConstructor carObj2 = new CallAConstrFromAnothConstructor(1969, "Ford", "Mustang"); // It will call the 2nd constructor (at line 10)
+        CallAConstrFromAnothConstructor carObj2 = new CallAConstrFromAnothConstructor(1969, "Ford", "Mustang"); // It will call the 2nd constructor (at line 12)
 
         carObj1.printCarInfo();
         carObj2.printCarInfo();
