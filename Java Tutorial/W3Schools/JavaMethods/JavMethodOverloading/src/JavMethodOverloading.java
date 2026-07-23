b8c0c1b86dd99eabda6cc87dadffcac7cd83283f
waseefmunir-max <waseef05@gmail.com>
2026-07-23 20:06:11 +0600

    JavMethodOverloading.java: Add overloaded sum2Numbers method for single double input

--- Java Tutorial/W3Schools/JavaMethods/JavMethodOverloading/src/JavMethodOverloading.java
+++ Java Tutorial/W3Schools/JavaMethods/JavMethodOverloading/src/JavMethodOverloading.java
@@ -7,9 +7,14 @@ public class JavMethodOverloading {
         return x + y;
     } // Method with the same name can be declared, as long as their data type is different
 
+    public static double sum2Numbers(double x) {
+        return x;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Sum of 2 integers: " + sum2Numbers(5, 6));
         System.out.println("Sum of 2 doubles: " + sum2Numbers(5.6, 6.5));
+        System.out.printf("Mirroring the number %.2f with the same method: %.2f\n", 4.5, sum2Numbers(4.5));
     }
 }
