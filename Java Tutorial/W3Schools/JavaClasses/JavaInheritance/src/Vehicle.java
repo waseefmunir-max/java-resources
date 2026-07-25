aa5345c3e24ebe0849b295e371b0080f6d826080
waseefmunir-max <waseef05@gmail.com>
2026-07-25 22:32:52 +0600

    Vehicle.java: Add Vehicle and Car classes; implement horn method and demonstrate inheritance

--- Java Tutorial/W3Schools/JavaClasses/JavaInheritance/src/Vehicle.java
+++ Java Tutorial/W3Schools/JavaClasses/JavaInheritance/src/Vehicle.java
@@ -1,2 +1,18 @@
-public class Vehicle {
+class Vehicle {
+    protected final String brandName = "BMW";
+
+    public void horn() {
+        System.out.println("Tuut, tuut!");
+    }
+}
+
+class Car extends Vehicle {
+    private final String modelName = "M3 GTR";
+
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        System.out.println(car.modelName + " " + car.brandName);
+        car.horn();
+    }
 }
