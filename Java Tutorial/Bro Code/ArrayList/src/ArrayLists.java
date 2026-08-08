5f025c1ac181792e839f7e61e45767d556479650
waseefmunir-max <waseef05@gmail.com>
2026-08-08 20:29:57 +0600

    ArrayLists.java: Enhance ArrayList methods with examples for .remove(), .set(), .get(), .size(), .contains(), .indexOf(), .lastIndexOf(), .isEmpty(), and .clear()

--- Java Tutorial/Bro Code/ArrayList/src/ArrayLists.java
+++ Java Tutorial/Bro Code/ArrayList/src/ArrayLists.java
@@ -37,30 +37,80 @@ public class ArrayLists {
 
         System.out.println();
 
+
         // .remove() method:
-        listFruit.remove(0); // removes "Mango" from the list
-        System.out.println(listFruit);
+//        listFruit.remove(0); // removes "Mango" from the list by index
+        listFruit.remove("Mango"); // removes "Mango" from the list by value
+        System.out.println("Removed 'Mango' from the array: " + listFruit);
+
+        System.out.println();
+
 
         // .set() method:
         listFruit.set(1, "Mango"); // replaces "Apple" with "Mango" at index 1
         System.out.println(listFruit);
 
+        System.out.println();
+
+
         // .get() method:
         System.out.println(listFruit.get(1));
+
+        System.out.println();
+
 
         // .size() method:
         System.out.println(listFruit.size()); // prints the size (number of elements) of the array
 
-        // Collections.sort(ArrayName) - Sorts an array alphanumerically
+        System.out.println();
+
         listFruit.remove(1);
         listFruit.add(0, "Mango");
         listFruit.add("Banana");
+        listFruit.add("Apple");
         listFruit.add("Orange");
+        listFruit.add("Watermelon");
         listFruit.add("Apple");
 
+        System.out.println("listFruit Array: \n" + listFruit);
+        System.out.println();
+
+
+        // .contains() method:
+        System.out.println(listFruit.contains("Watermelon")); // outputs 'true' if the listFruit contains "Watermelon"
+
+        System.out.println();
+
+
+        // .indexOf and .lastIndexOf() method:
+
+        System.out.println("Index: " + listFruit.indexOf("Watermelon")); // prints first index of Watermelon
+        System.out.println("Last index of Apple: " + listFruit.lastIndexOf("Apple"));
+
+        System.out.println();
+
+
+        // Collections.sort(ArrayName) - Sorts an array alphanumerically
         System.out.println(listFruit); // prints in default (by index) order
 
         Collections.sort(listFruit);
         System.out.println(listFruit); // prints the elements in alphanumerical order
+        System.out.println();
+
+
+        // .isEmpty() method:
+        System.out.println(listFruit.isEmpty()); // returns false as listFruit is not empty (even if it contains whitespaces, it won't be considered empty by .isEmpty() as .isEmpty() counts whitespaces)
+
+        System.out.println();
+
+
+        // .clear() method:
+        System.out.println("Before clearing: \n" + listFruit);
+
+        listFruit.clear(); // clearing the method
+
+        System.out.println("After clearing: \n" + listFruit);
+
+        System.out.println();
     }
 }
